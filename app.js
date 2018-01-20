0a059cb59573c53c30e12c59e8c22a39b691d9d1
LucasAlt <lucasalbert67@gmail.com>
2018-01-20 14:40:26 +0100

    Rewrite the code
    - Display information about the sender on discord and minecraft
    - Change the config structure

---
+++ app.js
@@ -0,0 +1,44 @@
+const mineflayer = require('mineflayer')
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+const config = require('./config.json')
+const prefix = config.prefix
+
+// Mineflayer connection configurtation
+const minecraft = mineflayer.createBot({
+  host: config.server.host,
+  port: config.server.port,
+  username: config.server.color + config.server.username,
+  colorEnabled: true
+})
+
+// When the bot is on
+client.on('ready', () => {
+  console.log(`Logged in as ${minecraft.username} on Minecraft!`)
+  console.log(`Logged in as ${client.user.tag} on Discord!`)
+  console.log('')
+})
+
+// Send message on discord
+client.on('message', message => {
+  if (message.content.startsWith(prefix)) {
+    const msgContent = message.author.username + ': ' + message.content.substr(prefix.length).trim()
+    minecraft.chat(msgContent)
+  }
+})
+
+minecraft.on('chat', function (username, message) {
+  if (config.server.color + username === minecraft.username) return
+  const sendMessage = '**' + username + ':**\n' + message
+  sendToDiscord(sendMessage)
+})
+
+// Function to get minecraft message on discord.
+function sendToDiscord (message) {
+  const log = client.channels.find('name', config.discordChannelName)
+  console.log('Send message !')
+  log.send(message)
+}
+
+client.login(config.token)
